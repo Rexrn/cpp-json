a364340515fde2c95de4701f78c9161f10f29bab
PoetaKodu <rfpawelsyska@gmail.com>
2018-09-14 23:01:12 +0200

    Reworked parse method with iterators.

--- include/RexrnCPPJSON/Parse.hpp
+++ include/RexrnCPPJSON/Parse.hpp
@@ -13,8 +13,23 @@ class Property;
 /// 	Exception that ocurred during and is directly associated with JSON parsing.
 /// </summary>
 struct ParseException : std::exception {
-    using std::exception::exception;
+	using std::exception::exception;
 };
+
+/// <summary>
+/// 	Parses JSON contents accessed using iterators from range [first, last).
+/// </summary>
+/// <typename name="TForwardIterator">Type of the forward iterator used to access JSON content.</typename> 
+/// <param name="first_">Param</param>
+/// <param name="last_">Param</param>
+/// <summary>
+/// 	ReturnDescription
+/// </summary>
+template <typename TForwardIterator>
+Property parse(TForwardIterator first_, TForwardIterator last_)
+{
+	throw std::logic_error("Not implemented.");
+}
 
 /// <summary>
 ///     Parses JSON contents stored within specified string.
@@ -24,6 +39,9 @@ struct ParseException : std::exception {
 ///     Root property of the specified JSON.
 /// </summary>
 template <typename TChar>
-Property parse(std::basic_string<TChar> const &JSONContents_);
+Property parse(std::basic_string<TChar> const &JSONContents_)
+{
+	return parse(JSONContents_.data(), JSONContents_.data() + JSONContents_.length());
+}
 
 }

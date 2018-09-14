8a8b612c729d8da51408ad5ff14778ff533f9a66
PoetaKodu <rfpawelsyska@gmail.com>
2018-09-15 00:00:04 +0200

    Included new files.

--- include/RexrnCPPJSON/Everything.hpp
+++ include/RexrnCPPJSON/Everything.hpp
@@ -3,5 +3,9 @@
 #include "Parse.hpp"
 
 #ifdef REXRN_CPP_JSON_HEADER_ONLY
-#include "../src/Parse.cpp"
+    // src
+    #include "../src/Parse.cpp"
+
+    // src/details:
+    #include "../src/details/String.cpp"
 #endif

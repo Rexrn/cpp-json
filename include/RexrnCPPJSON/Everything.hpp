8414688811b9e96961a4ccd692b3d1e5d27756d9
PoetaKodu <rfpawelsyska@gmail.com>
2018-09-15 17:36:57 +0200

    Updated file that includes entire library.

--- include/RexrnCPPJSON/Everything.hpp
+++ include/RexrnCPPJSON/Everything.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
 #include "Parse.hpp"
+#include "Property.hpp"
+#include "Value.hpp"
+#include "String.hpp"
 
 #ifdef REXRN_CPP_JSON_HEADER_ONLY
     // src
     #include "../src/Parse.cpp"
-
-    // src/details:
-    #include "../src/details/String.cpp"
+    #include "../src/Property.cpp"
+    #include "../src/Value.cpp"
+    #include "../src/String.cpp"
 #endif
